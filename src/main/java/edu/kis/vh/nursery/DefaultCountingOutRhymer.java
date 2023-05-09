f0f9bfb2993e142c92e18274fce7968b36f15e2e
jgrodzki <240675@edu.p.lodz.pl>
2023-05-09 15:54:00 +0200

    17. dodanie dokumentacji dla klasy DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,8 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Podstawowy Rhymer opearty o strukturę stosu LIFO.
+ */
 public class DefaultCountingOutRhymer {
 
     public static final int EMPTY_RHYMER_INDICATOR = -1;
@@ -11,29 +14,50 @@ public class DefaultCountingOutRhymer {
 
     private  int total = STACK_INITIAL_INDEX;
 
+    /**
+     * @return liczba elementów na stosie pomniejszona o jeden.
+     */
     public int getTotal() {
         return total;
     }
 
+    /**
+     * Dodaje element do stosu
+     * @param in dodawany element
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+    /**
+     * @return czy stos jest pusty
+     */
     public boolean callCheck() {
         return total == STACK_INITIAL_INDEX;
     }
 
+    /**
+     * @return czy stos jest pełny
+     */
     public boolean isFull() {
         return total == STACK_LAST_INDEX;
     }
 
+    /**
+     * Zwraca ostani element stosu bez usuwania go
+     * @return ostatni element stosu
+     */
     protected int peekaboo() {
         if (callCheck())
             return EMPTY_RHYMER_INDICATOR;
         return numbers[total];
     }
 
+    /**
+     * Zwraca ostani element stosu usuwając go
+     * @return ostatni element stosu
+     */
     public int countOut() {
         if (callCheck())
             return EMPTY_RHYMER_INDICATOR;
